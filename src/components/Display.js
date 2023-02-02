feee4ea20dd45a6c4f1d4829c583eb62263ad082
Furkan YILMAZ <59698269+frkanyilmaz2@users.noreply.github.com>
2023-02-02 16:26:40 +0300

    asdasd
    as

--- src/components/Display.js
+++ src/components/Display.js
@@ -5,7 +5,7 @@ const Display = ({second,session,start,startClock,reset,durum}) => {
     <div className='display-container'>
         <div className='displayer'>
             <h2 id='timer-label' style={{color:`${session  < 1 ? 'red': ''}`}}>{durum} </h2>
-            <p id='time-left' >{session<10 ? ('0'+session):session}:{second<10 ? ('0'+second) : second}</p>
+            <p id='time-left' style={{color:`${session  < 1 ? 'red': ''}`}} >{session<10 ? ('0'+session):session}:{second<10 ? ('0'+second) : second}</p>
         </div>
         <div className="play-reset-stop">
             <button className="start_stop" id="start_stop" onClick={startClock}><i className={`fa-sharp fa-solid fa-${start? 'pause':'play'}`}></i></button>
